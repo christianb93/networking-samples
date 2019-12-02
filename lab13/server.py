eddb015532a60ef3c276e6504c732ec7bad85db2
christianb93 <christian_bohr@web.de>
2019-12-02 21:01:33 +0100

    Only send short "Hello" message

--- lab13/server.py
+++ lab13/server.py
@@ -7,11 +7,7 @@ def application(environ, start_response):
         [('Content-type', 'text/html')]
     )
 
-    response = "<html><body><p><b>Environment data:</b></p>"
-    response += "<table><tr><th>Key</th><th>Value</th></tr>"
-    for key, value in environ.items():
-        response +=  "<tr><td>%s</td><td>%s</td></tr>" % ( key, value)
-    response = response + "</table></body></html>"
+    response = "<html><body><p>Hello!</p></body></html>"
     return [bytes(response, 'utf-8')]
 
 print("Starting up")
